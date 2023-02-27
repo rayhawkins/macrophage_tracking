9adba6c5492b33fda54cbfc791e03b69924f0dc6
rayhawkins <94799774+rayhawkins@users.noreply.github.com>
2023-02-27 15:40:29 -0500

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -1,1 +1,2 @@
 # Code here for performing thresholding and additional processes necesssary to segment nuclei
+# Input is raw images (4D grayscale images in x,y,z, and time), output is masks (3D binary images in x,y, and time)
