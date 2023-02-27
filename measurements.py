0b269de5e251baafc127a12388b8c9809170d285
rayhawkins <94799774+rayhawkins@users.noreply.github.com>
2023-02-27 15:44:17 -0500

    Update measurements.py

--- measurements.py
+++ measurements.py
@@ -1,1 +1,5 @@
 # Add code here for determining speed, directedness, number, etc. from tracked macrophages
+# Input is nuclei tracks (n by t by 2 numpy arrays where n is the number of macrophages, t is the number of timepoints, 
+# and 2 corresponds to (x,y) coordinates of that nucleus at that timepoint
+# Output is measurements in csv or mat and plots that allow visualization of the measured parameters
+
